0d09659334098c3bae2fb7f96f17267deaef3f1d
4everfree <32147862+4everfree@users.noreply.github.com>
2026-06-07 15:48:58 +0400

    Update ot.sh

--- ot.sh
+++ ot.sh
@@ -1,15 +1,26 @@
 #!/usr/bin/env bash
 
+# how to execute:  ./ot.sh  "name.mp4" "00:00:00:00,000"
 FILE="$1"
 TIME="$2"
 
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
 
+if [ -z "$TIME" ]; then
+  echo "Error: No time argument set!"
+  exit 1
+fi
+
+export TIME
 ot=$(python3 -c "
-t = '$TIME'.replace(',', ';').replace(';', ':').split(':')[::-1]
-print(sum(int(p) * w for p, w in zip(map(int, t), [1, 1000, 60000, 3600000])))
+import os
+t = os.environ['TIME'].replace(',', ';').replace(';', ':').split(':')[::-1]
+weights = [1, 1000, 60000, 3600000]
+print(sum(int(p) * w for p, w in zip(map(int, t), weights[:len(t)])))
 ")
+
+echo "Time offset (ms): $ot"
 
 ffmpeg -y -i "$FILE" -ar 16000 temporary.wav
 
